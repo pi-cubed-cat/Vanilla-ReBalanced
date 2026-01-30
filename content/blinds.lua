8beb644f4739a90eccd420c1d8bcc774736ebad3
pi-cubed-cat <xxcadengamingxx@gmail.com>
2026-01-30 14:13:19 +0800

    1.0.1a~BETA

--- content/blinds.lua
+++ content/blinds.lua
@@ -3,10 +3,10 @@ SMODS.Blind:take_ownership('final_vessel', { -- Violet Vessel
 }, false)
 
 SMODS.Blind:take_ownership('serpent', { -- The Serpent
-    calculate = function(self, blind, context)
+    --[[calculate = function(self, blind, context)
         if not blind.disabled then
             if context.drawing_cards and (G.GAME.current_round.hands_played ~= 0 or G.GAME.current_round.discards_used ~= 0) then
-                local num_cards = math.max(3, G.hand.config.card_limit - #G.hand.cards)
+                local num_cards = math.min(3, G.hand.config.card_limit - #G.hand.cards)
                 if num_cards > 0 then
                     return {
                         cards_to_draw = num_cards
@@ -14,5 +14,5 @@ SMODS.Blind:take_ownership('serpent', { -- The Serpent
                 end
             end
         end
-    end
+    end]]
 }, false)
